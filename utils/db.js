16e027e842313861b575553f47f1c5d2a827a5b3
fradojr <fradoforumm@gmail.com>
2021-06-22 20:31:43 +0700

    up

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://fradojr:frado007@cluster0.sfoui.mongodb.net/point-count?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://fradojr:frado007@cluster0.sfoui.mongodb.net/point-count?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
